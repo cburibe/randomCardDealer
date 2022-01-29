c269d24e06b4c2a4f4e5ec622dc23a69bc60d879
cburibe <curibepulgar@gmail.com>
2022-01-29 17:43:03 +0000

    project end

--- src/app.js
+++ src/app.js
@@ -6,6 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let generateRandomSuitCard = function() {
+    let cardSuits = ["diams", "hearts", "clubs", "spades"];
+    let indexCardsSuits = Math.floor(Math.random() * cardSuits.length);
+    return cardSuits[indexCardsSuits];
+  };
+  let generateRandomNumberCard = function() {
+    let cardNumbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexCardsNumbers = Math.floor(Math.random() * cardNumbers.length);
+    return cardNumbers[indexCardsNumbers];
+  };
+  document.querySelector(".card").className =
+    "card " + generateRandomSuitCard();
+  document.querySelector(".card").innerHTML = generateRandomNumberCard();
+
+  setInterval(function() {
+    document.querySelector(".card").className =
+      "card " + generateRandomSuitCard();
+    document.querySelector(".card").innerHTML = generateRandomNumberCard();
+  }, 10000);
 };
